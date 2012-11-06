7a209a1d3d4cd759fc1ffcf5926fb977b7f7635d
Lionel <lionel.jimenez@jwtdialogue.com>
2012-11-06 11:54:06 +0100

    Project

---
+++ App_Code/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Description résumée de Post
+/// </summary>
+public class Post
+{
+    public int Id { get; set; }
+    public string _Post { get; set; }
+    public int User { get; set; }
+    public string Categorie { get; set; }
+    public DateTime CreatedDate { get; set; }
+
+    public Post(int id, string post, int user, string categorie, DateTime createddate)
+    {
+        Id = id;
+        _Post = post;
+        User = user;
+        Categorie = categorie;
+        CreatedDate = createddate;
+    }
+}
